c0a4b3f8874ff3543c8b3c8b55d146b382385b2b
Vadimka Komissarov <vadimka_kom@mail.ru>
2017-02-25 20:07:17 +0300

    SOCKET was replaced in new file

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"sync"
-	"errors"
 
-	"net"
 	"net/http"
 
 	"os"
@@ -13,44 +11,6 @@ import (
 )
 import "github.com/gorilla/mux"
 
-
-
-type SockListener struct {
-	net.Listener
-	alive bool
-}
-
-func NewSockListener( proto, path string ) ( *SockListener, error ) {
-	s, e := net.Listen( proto, path ); if e != nil {
-		return nil,e
-	}
-	return &SockListener{
-		Listener: s,
-		alive: true,
-	}, nil
-}
-
-func ( sl *SockListener ) HTTPServe( r *mux.Router ) error {
-	if sl.alive == false { return errors.New("Listener is dead!") }
-
-	if e := http.Serve( sl.Listener, r ); e != nil {
-		switch sl.alive {
-		case true:
-			return e
-		default:
-			return nil
-		}
-	}
-	return nil
-}
-func ( sl *SockListener ) File() ( *os.File, error ) {
-	return sl.Listener.(*net.UnixListener).File()
-}
-func ( sl *SockListener ) Close() error {
-	if sl.alive == false { return errors.New("Listener is dead!") }
-	sl.alive = false
-	return sl.Listener.Close()
-}
 
 type App struct {
 	sync.WaitGroup
---
+++ socket.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"os"
+	"errors"
+
+	"net"
+	"net/http"
+)
+import "github.com/gorilla/mux"
+
+
+type SockListener struct {
+	net.Listener
+	alive bool
+}
+
+func NewSockListener( proto, path string ) ( *SockListener, error ) {
+	s, e := net.Listen( proto, path ); if e != nil {
+		return nil,e
+	}
+	return &SockListener{
+		Listener: s,
+		alive: true,
+	}, nil
+}
+func ( sl *SockListener ) HTTPServe( r *mux.Router ) error {
+	if sl.alive == false { return errors.New("Listener is dead!") }
+
+	if e := http.Serve( sl.Listener, r ); e != nil {
+		switch sl.alive {
+		case true:
+			return e
+		default:
+			return nil
+		}
+	}
+	return nil
+}
+func ( sl *SockListener ) File() ( *os.File, error ) {
+	return sl.Listener.(*net.UnixListener).File()
+}
+func ( sl *SockListener ) Close() error {
+	if sl.alive == false { return errors.New("Listener is dead!") }
+	sl.alive = false
+	return sl.Listener.Close()
+}
