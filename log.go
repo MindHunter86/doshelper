ece0a6ed23fe10bb789c949c9135b05a7c3d8fcf
Vadimka Komissarov <vadimka_kom@mail.ru>
2017-02-25 19:46:10 +0300

    Initial commit

---
+++ log.go
@@ -0,0 +1,69 @@
+package main
+
+
+import (
+	"os"
+	"log"
+	"sync"
+	"errors"
+)
+
+
+const (
+	LPFX_CORE = iota
+	LPFX_HTTPD
+)
+const (
+	LLEV_OK = iota
+	LLEV_NOT
+	LLEV_INF
+	LLEV_WRN
+	LLEV_ERR
+)
+
+type Logger struct {
+	*log.Logger
+	sync.RWMutex
+	Prefix uint8
+}
+
+
+func NewLogger( prefix uint8 ) *Logger {
+	return &Logger{
+		Logger: log.New( os.Stdout, "", log.Ldate | log.Ltime | log.Lmicroseconds ),
+		Prefix: prefix,
+	}
+}
+func ( l *Logger ) SetPrefix( p uint8 ) {
+	l.Lock(); l.Prefix = p; l.Unlock()
+}
+func ( l *Logger ) GetPrefix() string {
+	l.RLock()
+	defer l.RUnlock()
+
+	switch l.Prefix {
+	case LPFX_CORE:
+		return "\x1b[36;1m[CORE]:\x1b[0m"
+	case LPFX_HTTPD:
+		return "\x1b[36;1m[HTTPD]:\x1b[0m"
+	default:
+		return ""
+	}
+}
+func ( l *Logger ) PutOK( txt string ) {
+	l.Logger.Println( l.GetPrefix(), "\x1b[32;1m✔\x1b[0m", txt )
+}
+func ( l *Logger ) PutNon( txt string ) {
+	l.Logger.Println( l.GetPrefix(), "\x1b[31;1m✖\x1b[0m", txt )
+}
+func ( l *Logger ) PutInf( txt string ) {
+	l.Logger.Println( l.GetPrefix(), "\x1b[34;1m🛈\x1b[0m", txt )
+}
+func ( l *Logger ) PutWrn( txt string ) error {
+	l.Logger.Println( l.GetPrefix(), "\x1b[33;1m❢\x1b[0;33;22m", txt, "\x1b[0m" )
+	return errors.New(txt)
+}
+func ( l *Logger ) PutErr( txt string ) error {
+	l.Logger.Println( l.GetPrefix(), "\x1b[31;22m❢\x1b[0;31;1m", txt, "\x1b[0m" )
+	return errors.New(txt)
+}
---
+++ main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"sync"
+	"errors"
+
+	"net"
+	"net/http"
+
+	"os"
+	"os/signal"
+	"syscall"
+)
+import "github.com/gorilla/mux"
+
+
+
+type SockListener struct {
+	net.Listener
+	alive bool
+}
+
+func NewSockListener( proto, path string ) ( *SockListener, error ) {
+	s, e := net.Listen( proto, path ); if e != nil {
+		return nil,e
+	}
+	return &SockListener{
+		Listener: s,
+		alive: true,
+	}, nil
+}
+
+func ( sl *SockListener ) HTTPServe( r *mux.Router ) error {
+	if sl.alive == false { return errors.New("Listener is dead!") }
+
+	if e := http.Serve( sl.Listener, r ); e != nil {
+		switch sl.alive {
+		case true:
+			return e
+		default:
+			return nil
+		}
+	}
+	return nil
+}
+func ( sl *SockListener ) File() ( *os.File, error ) {
+	return sl.Listener.(*net.UnixListener).File()
+}
+func ( sl *SockListener ) Close() error {
+	if sl.alive == false { return errors.New("Listener is dead!") }
+	sl.alive = false
+	return sl.Listener.Close()
+}
+
+type App struct {
+	sync.WaitGroup
+	Socket *SockListener
+}
+
+func NewApp( netproto, netpath string ) ( *App, error ) {
+	s, e := NewSockListener( netproto, netpath ); if e != nil {
+		return nil,e
+	}
+	return &App{
+		Socket: s,
+	}, nil
+}
+func ( a *App ) Destroy() {
+	a.Socket.Close()
+}
+
+func main() {
+	const (
+		appNetProto string = "unix"
+		appNetPath string = "./dostest.sock"
+	)
+
+	l := NewLogger( LPFX_CORE )
+//	l.PutOK("Test message!")
+//	l.PutNon("Test message!")
+//	l.PutInf("Test message!")
+//	l.PutWrn("Test message!")
+//	l.PutErr("Test message!")
+
+
+	l.PutInf("Creating new app ...")
+	app, e := NewApp( appNetProto, appNetPath ); if e != nil {
+		l.PutErr("Could not create App: " + e.Error()); return
+	} else { l.PutOK("App has been created!") }
+	defer func() {
+		app.Wait()
+		app.Destroy()
+		l.PutOK("App has been destroyed!")
+	}()
+
+	l.PutInf("Kernel signal catcher initialisation ...")
+	var sgn = make( chan os.Signal )
+	signal.Notify( sgn, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL, syscall.SIGQUIT )
+	l.PutOK("Kernel signal catcher has been initialised!")
+
+
+	l.PutInf("Starting HTTP goroutine ...")
+	go func( a *App ) {
+		a.Add(1)
+		l := NewLogger( LPFX_HTTPD )
+		l.PutOK("HTTPD goroutine has been inited!")
+
+		r := NewHTTPRouter(l)
+		r.Router.HandleFunc( "/", r.WebRoot )
+
+		l.PutInf("Starting HTTP serving ...")
+		for i := uint8(0); i < uint8(4); i++ {
+			if e := a.Socket.HTTPServe( r.Router ); e != nil {
+				l.PutWrn("Pre-fail state! HTTPServe error: " + e.Error())
+				l.PutInf("Trying to restart HTTPServing ...")
+				continue;
+			}
+			break;
+		}
+
+		l.PutOK("HTTPD goroutine has been destroyed!")
+		a.Done()
+	}( app )
+
+	for {
+		select {
+		case <-sgn:
+			l.PutWrn("Catched QUIT signal from kernel! Stopping prg...")
+			app.Socket.Close()
+			return
+		}
+	}
+}
+
+type HTTPRouter struct {
+	Router *mux.Router
+	l *Logger
+}
+func NewHTTPRouter( l *Logger ) *HTTPRouter {
+	return &HTTPRouter{
+		Router: mux.NewRouter(),
+		l: l,
+	}
+}
+func ( hr *HTTPRouter ) WebRoot( w http.ResponseWriter, r *http.Request ) {
+	hr.l.PutOK("WebRoot: Request received !")
+	return
+}
