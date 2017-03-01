9c675a1d3a6e1758b1b08bd2c1967e311a9dcd8a
Vadimka Komissarov <vadimka_kom@mail.ru>
2017-03-01 20:00:11 +0300

    App replaced in new file

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"sync"
-
 	"net/http"
 
 	"os"
@@ -11,9 +9,6 @@ import (
 )
 import "github.com/gorilla/mux"
 
-//const (
-//	ERR_
-//)
 const (
 	ERR_SQL_CONNFAIL = "Connection status is not good!"
 	ERR_SQL_NOUSERS = "Could not find any user with "
@@ -32,51 +27,6 @@ const (
 ///		WARNING!!!!
 
 // Change mux? https://godoc.org/github.com/husobee/vestigo#CustomNotFoundHandlerFunc
-
-
-
-
-var app *App
-
-type App struct {
-	sync.WaitGroup
-	Users *UserBuf
-	Socket *SockListener
-}
-
-func NewApp() ( *App, error ) {
-	sl, e := NewSockListener( appNetProto, appNetPath ); if e != nil {
-		return nil,e
-	}
-	return &App{
-		Socket: sl,
-	}, nil
-}
-func ( a *App ) Destroy() {
-	a.Socket.Close()
-}
-func ( a *App ) ThreadHTTPD() {
-	a.Add(1)
-	l := NewLogger( LPFX_HTTPD )
-	l.PutOK("HTTPD goroutine has been inited!")
-
-	r := NewHTTPRouter(l)
-	r.Router.HandleFunc( "/", r.WebRoot )
-
-	l.PutInf("Starting HTTP serving ...")
-	for i := uint8(0); i < uint8(4); i++ {
-		if e := a.Socket.HTTPServe( r.Router ); e != nil {
-			l.PutWrn("Pre-fail state! HTTPServe error: " + e.Error())
-			l.PutInf("Trying to restart HTTPServing ...")
-			continue;
-		}
-		l.PutOK("HTTP serving has been stopped!")
-		break;
-	}
-
-	l.PutOK("HTTPD goroutine has been destroyed!")
-	a.Done()
-}
 
 
 func main() {
