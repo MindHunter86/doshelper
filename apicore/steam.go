e7a801812cc38fa4d3da515c8d961313aa2bc094
Vadimka Komissarov <vadimka_kom@mail.ru>
2017-05-27 03:35:40 +0300

    steam - fixed critical bugs; tested login

--- apicore/steam.go
+++ apicore/steam.go
@@ -2,7 +2,6 @@ package apicore
 
 import "errors"
 import "bytes"
-import "strings"
 import "regexp"
 
 import "github.com/valyala/fasthttp"
@@ -19,10 +18,10 @@ var (
 
 var (
 	steamoid_url []byte = []byte("https://steamcommunity.com/openid/login")
-	steamoid_mode []byte = []byte("checkid_setup[]byte(")
-	steamoid_ns []byte = []byte("http://specs.openid.net/auth/2.0[]byte(")
-	steamoid_identity []byte = []byte("http://specs.openid.net/auth/2.0/identifier_select[]byte(")
-	steamoid_params []byte = []byte("assoc_handle,signed,sig,ns[]byte(")
+	steamoid_mode []byte = []byte("checkid_setup")
+	steamoid_ns []byte = []byte("http://specs.openid.net/auth/2.0")
+	steamoid_identity []byte = []byte("http://specs.openid.net/auth/2.0/identifier_select")
+	steamoid_params []byte = []byte("assoc_handle,signed,sig,ns")
 	steamoid_valide *regexp.Regexp = regexp.MustCompile("^(http|https)://steamcommunity.com/openid/id/[0-9]{15,25}$")
 	steamoid_dightex *regexp.Regexp = regexp.MustCompile("\\D+")
 )
@@ -34,13 +33,14 @@ type steamOid struct {
 func (self *steamOid) configure( uri *fasthttp.URI, header fasthttp.RequestHeader ) ( *steamOid, error ) {
 	if self == nil { return nil,err_SteamOid_NullSelf }
 
-	self.realm = uri.RequestURI()
-	if i := strings.Index( string(self.realm), "openid" ); i != -1 { self.realm = self.realm[0: i-1] }
+	self.realm = append( uri.Scheme(), []byte("://") ...)
+	self.realm = append( self.realm, header.Peek("X-Forwarded-Host") ...)
 
-	self.return_to = append( uri.Scheme(), header.Peek("X-Forwarded-Host")... )
-	self.return_to = append( self.return_to, self.realm... )
+	self.return_to = uri.RequestURI()
+	if i := bytes.Index( self.return_to, []byte("openid") ); i != -1 { self.return_to = self.return_to[0:i-1] }
+	self.return_to = append( self.realm, self.return_to ...)
+
 	self.data = uri.QueryArgs()
-
 	return self,nil
 }
 func (self *steamOid) authUrl() []byte {
@@ -56,7 +56,7 @@ func (self *steamOid) authUrl() []byte {
 	var i uint8
 	var query []byte
 	for k,v := range oid_data {
-		query = append( []byte( k + "=" ), v ...)
+		query = append( query,  append( []byte( k + "=" ), v ...) ...)
 		if i != uint8( len(oid_data) - 1 ) {
 			query = append( query, []byte("&") ...)
 		}
@@ -66,12 +66,8 @@ func (self *steamOid) authUrl() []byte {
 	return append( steamoid_url, query ...)
 }
 func (self *steamOid) validate() ( []byte, error ) {
-	if bytes.Equal( self.data.Peek("openid.mode"), []byte("id_res") ) == false {
-		return nil, err_SteamOid_InvalidMode
-	}
-	if bytes.Equal( self.data.Peek("openid.return_to"), self.return_to ) == false {
-		return nil, err_SteamOid_InvalidReturn
-	}
+	if ! bytes.Equal( self.data.Peek("openid.mode"), []byte("id_res") ) { return nil, err_SteamOid_InvalidMode }
+	if ! bytes.Equal( self.data.Peek("openid.return_to"), self.return_to ) { return nil, err_SteamOid_InvalidReturn }
 
 	postform := fasthttp.AcquireArgs()
 	for _,v := range bytes.Split( steamoid_params, []byte(",") ) {
@@ -87,35 +83,9 @@ func (self *steamOid) validate() ( []byte, error ) {
 	if rspCode != 200 { return nil,err_SteamOid_SteamNon200 }
 
 	response := bytes.Split( rspBody, []byte("\n") )
-	if bytes.Equal( response[0], append( []byte("ns:"), steamoid_ns ...) ) == false {
-		return nil,err_SteamOid_InvalidNS
-	}
+	if ! bytes.Equal( response[0], append( []byte("ns:"), steamoid_ns ...) ) { return nil,err_SteamOid_InvalidNS }
 	if bytes.HasSuffix( response[1], []byte("false") ) { return nil,err_SteamOid_NonValidate }
 
-	if steamoid_valide.Match( self.data.Peek("openid.claimed_id") ) { return nil,err_SteamOid_InvalidPattern }
+	if ! steamoid_valide.Match( self.data.Peek("openid.claimed_id") ) { return nil,err_SteamOid_InvalidPattern }
 	return steamoid_dightex.ReplaceAll( self.data.Peek("openid.claimed_id"), []byte("") ), nil
-
-
-
-
-
-//	params := make(url.Values)
-//	for _,v := range strings.Split( steamoid_params, "," ) {
-//		params.Set( "openid." + v, string(self.data.Peek("openid." + v)) )
-//	}
-//	for _,v := range strings.Split( self.data.Peek("openid.signed"), "," ) {
-//		params.Set( "openid." + v, string(self.data.Peek("openid." + v)) )
-//	}
-//	params.Set("openid.mode", "check_authentication")
-//
-//	rsp, e := http.PostForm( steamoid_url, params ); if e != nil { return "",e }
-//	defer rsp.Body.Close()
-//	rspContent, e := ioutil.ReadAll(rsp.Body); if e != nil { return "",e }
-//
-//	response := strings.Split( string(rspContent), "\n" )
-//	if response[0] != "ns:" + steamoid_ns { return "",err_SteamOid_InvalidNS }
-//	if strings.HasSuffix( response[1], "false" ) { return "",err_SteamOid_NonValidate }
-//
-//	if steamoid_valide.MatchString(self.data.Peek("openid.claimed_id")) { return "", err_SteamOid_InvalidPattern }
-//	return steamoid_dightex.ReplaceAllString( self.data.Peek("openid.claimed_id"), "" ),nil
 }
