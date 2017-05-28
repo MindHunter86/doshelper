9d1d8e4868c22afe1c0d658164ed600c6e17a12d
Vadimka Komissarov <vadimka_kom@mail.ru>
2017-05-28 04:57:17 +0300

    some changes in login handler

--- apicore/handler.go
+++ apicore/handler.go
@@ -13,12 +13,25 @@ func (self *handler) Login( ctx *fasthttp.RequestCtx ) {
 
 	switch string(steamoid.data.Peek("openid.mode")) {
 	case "":
-		ctx.Redirect( string(steamoid.authUrl()), 302 )
+//		ctx.Redirect( string(steamoid.authUrl()), 302 )
+		ctx.Write(steamoid.authUrl())
 	case "cancel":
 		ctx.Write([]byte("Authorization cancelled!"))
 	default:
-		if steamid, e := steamoid.validate(); e != nil {
+		steamid, e := steamoid.validate(); if e != nil {
 			ctx.Error( e.Error(), fasthttp.StatusInternalServerError )
-		} else { ctx.Write([]byte(steamid)) }
+		}
+		ctx.Write([]byte(steamid))
+
+		var cookie *fasthttp.Cookie = fasthttp.AcquireCookie()
+		cookie.SetKey("claimed_id")
+		cookie.SetPath("/")
+		cookie.SetValueBytes(steamid)
+		cookie.SetDomain(".gotest.mh00.info")
+		cookie.SetSecure(false)
+		cookie.SetHTTPOnly(true)
+
+		ctx.Response.Header.SetCookie(cookie)
+		ctx.Redirect("http://golucky.gotest.mh00.info/", 307)
 	}
 }
